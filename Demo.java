df49ccdf352c377c4a64b504dae0bd7994d03b27
Umabharati11 <119678200+Umabharati11@users.noreply.github.com>
2023-06-28 10:15:50 +0530

    First git
    to test

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+  public static void main(String[] args)
+   {
+      int a = 10;
+      int b;
+      b = a++ + ++a;
+      System.out.println(a);
+      System.out.println(b);
+    }
+
+}
